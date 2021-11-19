213122e50f14dd2d43d9e497414b14cb2c65b287
071sh <shayan_safaeiy@yahoo.com>
2021-11-19 15:23:38 +0330

    Implementation structure

---
+++ Admin.EndPoint/Program.cs
@@ -0,0 +1,52 @@
+﻿using Infrastructure.IdentityConfigs;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Contexts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+#region ConnectionString
+
+string connection =builder.Configuration["ConnectionString:SqlServer"];
+builder.Services.AddDbContext<DataBaseContext>(option => option.UseSqlServer(connection));
+
+builder.Services.AddIdentityConfig(builder.Configuration);
+
+builder.Services.AddAuthorization();
+builder.Services.ConfigureApplicationCookie(option =>
+{
+    option.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+    option.LoginPath = "/account/login";
+    option.AccessDeniedPath = "/Account/AccessDenied";
+    option.SlidingExpiration = true; // اگر کاربر فعالیت داشت 10 دقیقه تمدید میشه و الله خارج میشه
+});
+
+#endregion
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Application/IDataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public interface IDataBaseContext
+    {
+        #region SaveChange
+
+        int SaveChanges();
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+
+        #endregion
+    }
+}
