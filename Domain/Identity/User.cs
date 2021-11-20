7f7307fac9290591b37f25a53d203ca33b0d1f01
071sh <shayan_safaeiy@yahoo.com>
2021-11-20 21:33:46 +0330

    Identity

--- Domain/Identity/User.cs
+++ Domain/Identity/User.cs
@@ -9,5 +9,7 @@ namespace Domain.Identity
 {
     public class User : IdentityUser<int>
     {
+        public string Name { get; set; }
+        public string Family { get; set; }
     }
 }
--- Persistence/Contexts/IdentityDataBaseContext.cs
+++ Persistence/Contexts/IdentityDataBaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -14,6 +15,134 @@ namespace Persistence.Contexts
         public IdentityDataBaseContext(DbContextOptions<IdentityDataBaseContext> options) : base(options)
         {
 
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>(b =>
+            {
+                // Primary key
+                b.HasKey(u => u.Id);
+
+                // Indexes for "normalized" username and email, to allow efficient lookups
+                b.HasIndex(u => u.UserName).HasName("UserNameIndex").IsUnique();
+                b.HasIndex(u => u.Email).HasName("EmailIndex");
+
+                // Maps to the AspNetUsers table
+                b.ToTable("AspNetUsers");
+
+                // A concurrency token for use with the optimistic concurrency checking
+                b.Property(u => u.ConcurrencyStamp).IsConcurrencyToken();
+
+                // Limit the size of columns to use efficient database types
+                b.Property(u => u.UserName).HasColumnType("VARCHAR").HasMaxLength(10);
+                //b.Property(u => u.NormalizedUserName).HasMaxLength(256);
+                b.Property(u => u.Email).HasColumnType("VARCHAR").HasMaxLength(256);
+                //b.Property(u => u.NormalizedEmail).HasMaxLength(256);
+                b.Property(u=>u.Name).HasMaxLength(256);
+                b.Property(u => u.Family).HasMaxLength(256);
+                b.Property(u => u.PhoneNumber).HasColumnType("VARCHAR").HasMaxLength(256);
+
+                b.Ignore(u => u.NormalizedEmail);
+                b.Ignore(u => u.NormalizedUserName);
+
+                // The relationships between User and other entity types
+                // Note that these relationships are configured with no navigation properties
+
+                // Each User can have many UserClaims
+                b.HasMany<IdentityUserClaim<int>>().WithOne().HasForeignKey(uc => uc.UserId).IsRequired();
+
+                // Each User can have many UserLogins
+                b.HasMany<IdentityUserLogin<int>>().WithOne().HasForeignKey(ul => ul.UserId).IsRequired();
+
+                // Each User can have many UserTokens
+                b.HasMany<IdentityUserToken<int>>().WithOne().HasForeignKey(ut => ut.UserId).IsRequired();
+
+                // Each User can have many entries in the UserRole join table
+                b.HasMany<IdentityUserRole<int>>().WithOne().HasForeignKey(ur => ur.UserId).IsRequired();
+            });
+
+            builder.Entity<IdentityUserClaim<int>>(b =>
+            {
+                // Primary key
+                b.HasKey(uc => uc.Id);
+
+                // Maps to the AspNetUserClaims table
+                b.ToTable("AspNetUserClaims");
+            });
+
+            builder.Entity<IdentityUserLogin<int>>(b =>
+            {
+                // Composite primary key consisting of the LoginProvider and the key to use
+                // with that provider
+                b.HasKey(l => new { l.LoginProvider, l.ProviderKey });
+
+                // Limit the size of the composite key columns due to common DB restrictions
+                b.Property(l => l.LoginProvider).HasMaxLength(128);
+                b.Property(l => l.ProviderKey).HasMaxLength(128);
+
+                // Maps to the AspNetUserLogins table
+                b.ToTable("AspNetUserLogins");
+            });
+
+            builder.Entity<IdentityUserToken<int>>(b =>
+            {
+                // Composite primary key consisting of the UserId, LoginProvider and Name
+                b.HasKey(t => new { t.UserId, t.LoginProvider, t.Name });
+
+                // Limit the size of the composite key columns due to common DB restrictions
+                b.Property(t => t.LoginProvider).HasMaxLength(128);
+                b.Property(t => t.Name).HasMaxLength(128);
+
+                // Maps to the AspNetUserTokens table
+                b.ToTable("AspNetUserTokens");
+            });
+
+            builder.Entity<Role>(b =>
+            {
+                // Primary key
+                b.HasKey(r => r.Id);
+
+                // Index for "normalized" role name to allow efficient lookups
+                b.HasIndex(r => r.NormalizedName).HasName("RoleNameIndex").IsUnique();
+
+                // Maps to the AspNetRoles table
+                b.ToTable("AspNetRoles");
+
+                // A concurrency token for use with the optimistic concurrency checking
+                b.Property(r => r.ConcurrencyStamp).IsConcurrencyToken();
+
+                // Limit the size of columns to use efficient database types
+                b.Property(u => u.Name).HasMaxLength(256);
+                b.Property(u => u.NormalizedName).HasMaxLength(256);
+
+                // The relationships between Role and other entity types
+                // Note that these relationships are configured with no navigation properties
+
+                // Each Role can have many entries in the UserRole join table
+                b.HasMany<IdentityUserRole<int>>().WithOne().HasForeignKey(ur => ur.RoleId).IsRequired();
+
+                // Each Role can have many associated RoleClaims
+                b.HasMany<IdentityRoleClaim<int>>().WithOne().HasForeignKey(rc => rc.RoleId).IsRequired();
+            });
+
+            builder.Entity<IdentityRoleClaim<int>>(b =>
+            {
+                // Primary key
+                b.HasKey(rc => rc.Id);
+
+                // Maps to the AspNetRoleClaims table
+                b.ToTable("AspNetRoleClaims");
+            });
+
+            builder.Entity<IdentityUserRole<int>>(b =>
+            {
+                // Primary key
+                b.HasKey(r => new { r.UserId, r.RoleId });
+
+                // Maps to the AspNetUserRoles table
+                b.ToTable("AspNetUserRoles");
+            });
+            //base.OnModelCreating(builder);
         }
     }
 }
