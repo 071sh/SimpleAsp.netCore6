857fa31f7d6a091e8eb3ea13b1bdcf35cdd9ddb6
071sh <shayan_safaeiy@yahoo.com>
2021-11-24 13:01:24 +0330

    menu

--- Site.EndPoint/Program.cs
+++ Site.EndPoint/Program.cs
@@ -6,5 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+//if (builder.HostEnvironment.IsDevelopment())
+//{
+//}
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 await builder.Build().RunAsync();
